f51730350af4d9a1bbbb69f2d751f7084c6784c2
Beka <bektemir.abdymomunov@mail.ru>
2021-01-13 10:33:41 -0500

    question field (step 4)

--- src/Bektemir/Create_A_Poll.java
+++ src/Bektemir/Create_A_Poll.java
@@ -7,5 +7,7 @@ public class Create_A_Poll {
         System.out.println("type some message into message field");
 
         System.out.println("choose who i will send");
+
+        System.out.println("Type the question in the question field");
     }
 }
