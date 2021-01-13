cd45bc8c2b569948e799c20bdc3a913b067d6078
Beka <bektemir.abdymomunov@mail.ru>
2021-01-13 10:31:36 -0500

    1 step in test case(type mssg into mssg field)

--- src/Bektemir/Create_A_Poll.java
+++ src/Bektemir/Create_A_Poll.java
@@ -3,5 +3,7 @@ package Bektemir;
 public class Create_A_Poll {
     public static void main(String[] args) {
         System.out.println("click poll button here");
+
+        System.out.println("type some message into message field");
     }
 }
